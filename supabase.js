5a877eb994c6b77c56afbbdbf137909eb14907a5
Rasmus <rame0001@stud.kea.dk>
2024-09-10 10:47:01 +0200

    Fetch Project

---
+++ supabase.js
@@ -0,0 +1,18 @@
+fetch("https://wqcieablytxowrowovbq.supabase.co/rest/v1/T&S", {
+    method:"GET",
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc"
+    }
+})
+
+    .then(res=>res.json())
+    .then(showData);
+
+
+function showData(items) {
+    console.log(items);
+   
+   // items.forEach()
+}
+
+//function (){}
