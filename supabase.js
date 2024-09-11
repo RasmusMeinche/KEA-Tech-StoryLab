04931a31398b6be8381e5c465096dcaec7bd5150
RasmusMeinche <rame0001@stud.kea.dk>
2024-09-12 00:25:20 +0200

    Data bliver hentet og fungerer

--- supabase.js
+++ supabase.js
@@ -1,3 +1,6 @@
+// url = https://wqcieablytxowrowovbq.supabase.co
+//api key = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc
+
 fetch("https://wqcieablytxowrowovbq.supabase.co/rest/v1/T&S", {
     method:"GET",
     headers: {
