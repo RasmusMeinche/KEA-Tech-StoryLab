be6d6ac04a1750b14f8c3f508edb8b46f6304d14
Rasmus <rame0001@stud.kea.dk>
2024-09-17 12:48:46 +0200

    Lidt rettelser

--- productlist.js
+++ productlist.js
@@ -1,12 +1,12 @@
 //Når hele HTML-Dokumentet er indlæst, bliver funktionen init kaldt 
-window.addEventListener("DOMContentLoaded", init);
+/* window.addEventListener("DOMContentLoaded", init); */
 
 
 //Kode for kategoriliste
 const categoryList = document.querySelector("#categorylist");
 const uniqueCategories = new Set(); // Opretter et tomt Set til at holde unikke kategorier
 
-fetch("https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc&select=Taksonomi1", {
+fetch("https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?select=Taksonomi1", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc"
@@ -37,14 +37,12 @@ Remote-controlled & Robot Devices
 const params = new URLSearchParams(document.location.search);
 const category = params.get("category");
 
+console.log(category);
+
 let url = undefined;
 
 // Tjek om kategori-parametret er til stede i URL'en
-if (params.has("category")) {
-  url = `https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc&Taksonomi1=eq.${category}`;
-} else {
-  url = "https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc&Taksonomi1";
-}
+  url = `https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?select=${category}`
 
 function init() {
   const productList = document.querySelector("#productTemplate");
