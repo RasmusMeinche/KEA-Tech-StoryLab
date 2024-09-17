7024d9a914c3b87a3974ea8cdf7b95764d12d522
Rasmus <rame0001@stud.kea.dk>
2024-09-17 14:41:22 +0200

    FUCKING VIRKER MAN

--- productlist.js
+++ productlist.js
@@ -1,6 +1,7 @@
 //Når hele HTML-Dokumentet er indlæst, bliver funktionen init kaldt 
-/* window.addEventListener("DOMContentLoaded", init); */
+window.addEventListener("DOMContentLoaded", init);
 
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc`;
 
 //Kode for kategoriliste
 const categoryList = document.querySelector("#categorylist");
@@ -9,7 +10,7 @@ const uniqueCategories = new Set(); // Opretter et tomt Set til at holde unikke 
 fetch("https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?select=Taksonomi1", {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc"
+    apikey: key
   }
 })
   .then((response) => response.json())
@@ -32,37 +33,36 @@ Computers & Peripheral Devices
 Remote-controlled & Robot Devices
  */
 
-
 // Hent kategori-parametret fra URL'en (husk at det er 'category' her, ikke 'Taksonomi1')
-const params = new URLSearchParams(document.location.search);
-const category = params.get("category");
 
-console.log(category);
+let url, productList, productTemplate;
 
-let url = undefined;
-
+function init () {
+  const params = new URLSearchParams(document.location.search);
+  const category = params.get("category");
+  url = `https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?Taksonomi1=eq.${category}`
+  console.log(url);
+  getData();
+}
 // Tjek om kategori-parametret er til stede i URL'en
-  url = `https://wqcieablytxowrowovbq.supabase.co/rest/v1/T%26S?select=${category}`
 
-function init() {
-  const productList = document.querySelector("#productTemplate");
-  const productTemplate = document.querySelector("template").content;
+function getData() {
+productList = document.querySelector("#productListSection");
+productTemplate = document.querySelector("#second").content;
 
   fetch(url, {
     method: "GET",
     headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxY2llYWJseXR4b3dyb3dvdmJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4OTY4NDMsImV4cCI6MjA0MTQ3Mjg0M30.SJtYsRbBQPSJuze0h2FncM1plrOh-QLb9N3mfSNjeQc"
+      apikey: key
     }
   })
   .then((res) => res.json())
-  .then((json) => {
-    console.log("JSON response:", json);
-    json.forEach(showProduct);
-  });
+  .then((json) => json.forEach(showProduct));
 }
 
 function showProduct(product) {
+  console.log(product)
   const clone = productTemplate.cloneNode(true);
-  clone.querySelector("h3").textContent = product.Taksonomi1;
-  productList.appendChild(clone);
+  clone.querySelector("h3").textContent = product.Type; 
+  productList.appendChild(clone); 
 }
